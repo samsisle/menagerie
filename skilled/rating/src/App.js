16d4391854695058fa47f9139f3ece484f762045
samsisle <samko9522@gmail.com>
2019-07-25 14:06:48 -0700

    🌴 Remove extra div

--- skilled/rating/src/App.js
+++ skilled/rating/src/App.js
@@ -2,12 +2,6 @@ import React from 'react';
 
 import Stars from './components/Stars';
 
-import './css/App.css';
-
 export default function App() {
-  return (
-    <div className="App">
-      <Stars />
-    </div>
-  );
+  return <Stars />;
 }
