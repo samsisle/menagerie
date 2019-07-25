b8e902a96f9b8c5eee57c19403633526cc16611d
samsisle <samko9522@gmail.com>
2019-07-25 14:07:29 -0700

    🌳 Add comments (source, author, date, etc.)

--- skilled/rating/src/index.js
+++ skilled/rating/src/index.js
@@ -1,3 +1,13 @@
+/**
+ * Source : https://www.skilledinc.com/app/feedback-details/2243
+ * Author : Samuel Ko
+ * Date   : 2019-07-16
+ *
+ * Self-assessment:
+ * I didn't do too hot on this one. Most of the work on here is
+ * post work to get a hang of the stuff I should have been done.
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -6,7 +16,7 @@ import * as serviceWorker from './lib/serviceWorker';
 
 import './css/index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('rating'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
