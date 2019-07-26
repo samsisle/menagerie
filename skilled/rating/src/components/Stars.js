422f0b103d3f1542bbb726614b282e4a45807042
samsisle <samko9522@gmail.com>
2019-07-25 22:27:59 -0700

    🌟 Add half-star rating logic

--- skilled/rating/src/components/Stars.js
+++ skilled/rating/src/components/Stars.js
@@ -34,23 +34,49 @@ export default function Stars() {
     <>
       <div className="stars">
         {stars.map(star => (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill={hover >= star.id ? '#1da1f2' : rating >= star.id ? '#51bb7b' : '#808080'}
-            stroke={hover >= star.id ? '#1da1f2' : rating >= star.id ? '#51bb7b' : '#808080'}
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            onClick={handleRating(star.id)}
-            onMouseEnter={handleMouseEnter(star.id)}
-            onMouseLeave={handleMouseLeave}
-            key={star.id}
-          >
-            <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
-          </svg>
+          <div className="star" key={star.id}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="12"
+              height="24"
+              onClick={handleRating(star.left)}
+              onMouseEnter={handleMouseEnter(star.left)}
+              onMouseLeave={handleMouseLeave}
+            >
+              <polygon
+                fill={hover >= star.left ? '#1da1f2' : rating >= star.left ? '#51bb7b' : '#808080'}
+                stroke={
+                  hover >= star.left ? '#1da1f2' : rating >= star.left ? '#51bb7b' : '#808080'
+                }
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                points="12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="12"
+              height="24"
+              viewBox="12 0 12 24"
+              onClick={handleRating(star.right)}
+              onMouseEnter={handleMouseEnter(star.right)}
+              onMouseLeave={handleMouseLeave}
+            >
+              <polygon
+                fill={
+                  hover >= star.right ? '#1da1f2' : rating >= star.right ? '#51bb7b' : '#808080'
+                }
+                stroke={
+                  hover >= star.right ? '#1da1f2' : rating >= star.right ? '#51bb7b' : '#808080'
+                }
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77"
+              />
+            </svg>
+          </div>
         ))}
       </div>
       <button className="reset" onClick={handleReset}>
