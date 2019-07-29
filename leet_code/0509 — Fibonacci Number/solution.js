63039197b28803ff709cf6aca14c186dca0399b8
samsisle <samko9522@gmail.com>
2019-07-29 00:01:56 -0700

    ✨Remove console.logs

--- leet_code/0509 — Fibonacci Number/solution.js
+++ leet_code/0509 — Fibonacci Number/solution.js
@@ -10,14 +10,8 @@
 function fib(n) {
   let memo = {};
   for (let i = 0; i <= n; i++) {
-    if (i <= 1) {
-      memo[i] = i;
-      console.log(`memo[${i}] = ${memo[i]}\n`);
-    }
-    else {
-      memo[i] = memo[i - 1] + memo[i - 2];
-      console.log(`memo[${i}] = ${memo[i]}\n`);
-    }
+    if (i <= 1) memo[i] = i;
+    else memo[i] = memo[i - 1] + memo[i - 2];
   }
   return memo[n];
 }
