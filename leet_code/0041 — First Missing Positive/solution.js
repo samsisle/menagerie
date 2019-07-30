63c3bb9aa9e0f8956cb55854dbf5cd952d1f82a8
samsisle <samko9522@gmail.com>
2019-07-30 14:28:19 -0700

    ✨ Update solution.js

--- leet_code/0041 — First Missing Positive/solution.js
+++ leet_code/0041 — First Missing Positive/solution.js
@@ -10,7 +10,7 @@
 const performance = require('../../lib/performance');
 
 function firstMissingPositive(nums) {
-  let map = new Map(); // using new ES6 new Map
+  let map = new Map(); // using ES6 new Map
   for (let num of nums) map.set(num, true); // stores the value by key ... map.set(key, value)
   for (let i = 1; i <= Number.MAX_SAFE_INTEGER; i++) {
     if (!map.has(i)) return i; // if the map doesn't have i, return i
