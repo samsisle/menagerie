b9b9dc6bf6547707fdb4964f8f255ea647ba3f29
samsisle <samko9522@gmail.com>
2019-08-01 14:51:20 -0700

    ✨ Add solution.js

---
+++ leet_code/0876 — Middle of Linked List/solution.js
@@ -0,0 +1,46 @@
+/**
+ * Source : https://leetcode.com/problems/middle-of-the-linked-list/
+ * Author : Samuel Ko
+ * Date   : 2019-08-01
+ *
+ * Time Complexity  : O(n)
+ * Space Complexity : O(1)
+ */
+
+const performance = require('../../performance');
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+function middleNode(head) {
+  let slow = head;
+  let fast = head;
+  if (!head) return;
+  // this specifc conditional is needed so the while loop
+  // stops before reaching a TypeError
+  while (fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  if (fast.next) return slow.next;
+  else return slow;
+}
+
+const a = new Node(1);
+const b = new Node(2);
+const c = new Node(3);
+const d = new Node(4);
+const e = new Node(5);
+const f = new Node(6);
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+e.next = f;
+
+performance.test(() => middleNode(a));
