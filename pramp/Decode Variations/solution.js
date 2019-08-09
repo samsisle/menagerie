23623f0938b20111ba5e423ccbb621efc4764d66
samsisle <samko9522@gmail.com>
2019-08-08 19:48:41 -0700

    ✨ Update solution.js

--- pramp/Decode Variations/solution.js
+++ pramp/Decode Variations/solution.js
@@ -1,104 +1,45 @@
 /**
  * Source : https://www.pramp.com/challenge/r1Kw0vwG6OhK9AEGAy6L
  * Author : Samuel Ko
- * Date   : 2019-07-19
+ * Date   : 2019-07-19, 2019-08-08
  *
- * Time Complexity  :  ?
- * Space Complexity :  ?
+ * Time Complexity  :  O(n)
+ * Space Complexity :  O(1)
  */
 
-// function decodeVariations(S, INDEX START) {
-// 	/**
-// 	@param S: string
-// 	@return: integer
-// 	*/
-  // return Recur(S,0);
-// }
- 
-// Recur(S, int startIndex)
-//   {
-//     if(map.contains(S))
-//       return  map.get();
-//     int result=0;
-//     if(startIndex==s.length()-1 || startIndex+1==s.length()-1)
-//       return 1;
+const performance = require('../../performance');
 
-//       result+=Recur(S,startIndex+1);
-//     if(S.beginswithNumLessThanEquals26()
-//     result+=Recur(S,startIndex+2);
-//   }
+/**
+ * @param {string} s
+ * @return {number}
+ */
+function numDecodings(s) {
+  // not sure where you get the logic to start both at 1 and 1,
+  // but the other solutions do something similar -> [1, 1, 0, 0, 0]
+  let x = 1;
+  let y = parseInt(s[0]) > 0 ? 1 : 0;
+  let output = y;
 
-  //620
-  //6 2 0
-  //6 2 2, 6 22 -->2 ways
-  //6 (27) 2 7 ==>1 way as result
-// function decodeVariations(S):
-//     N = S.length
-//     dp = new Array(N)
-//     dp[N] = 1 // value of dp at i is number of ways you can decode the string starting at index i
-     
-//     for i from N-1 to 0:
-//         if S[i] == '0':
-//             dp[i] = 0
-//         else if S[i] == '1':
-//             dp[i] = dp[i+1] + dp[i+2]
-//         else if S[i] == '2':
-//             dp[i] = dp[i+1]
-//             if i+1 < S.length && S[i+1] <= '6':
-//                 dp[i] += dp[i+2]
-//         else:
-//             dp[i] = dp[i+1]
+  for (let i = 2; i <= s.length; i++) {
+    // for example, in '1262' check 2 and 12, then add to the
+    // growing output; x lags behind y, while y becomes the output
+    output =
+      (isDecodable(s.substring(i - 1, i)) ? y : 0) +
+      (isDecodable(s.substring(i - 2, i)) ? x : 0);
+    x = y;
+    y = output;
+  }
 
-//     return dp[0]
+  return output;
+}
 
+function isDecodable(s) {
+  const num = parseInt(s);
+  if (s.length === 1) return num > 0;
+  return num >= 10 && num <= 26;
+}
 
+// where do you find clever solutions like this in one pressure interview setting?
+// guess I need more practice...
 
-//1272
-
-// A 272
-//   B 72
-//     G 2
-//       B
-
-// L 72
-//   G 2
-//     B
-    
-//     return result
-
-// 1262
-// A 262
-//   B 62
-//     F 2
-//       B
-//   Z 2
-//     B
-// L 62
-//   F 2
-//     B
-    
-// 3287489237489237 -->already solved.
-// optimization save it.
-// Map<String, numWays(Integer)>
-// 2, B
-// 262 BFB, ZB
-// 1262 ABFB, AZB, LFB
-
-// 1262, 3
-
-    
-    
-//   Recur(622, 0)
-//     Recur(622,1)
-//       START+1
-//       RECUR(622,2)
-//         RETURN 1
-//       RECUR(622,3)
-//         RETURN 1
-  
-  
-  //2 ---> 1 way 
-  //34 ---> 1 way , 25 ---> 2 ways
-  //3 digits ---> 1 digit (2digit) or 2digits (1digit)
-// 	return output;
-// }
+performance.test(() => numDecodings('1262'));
