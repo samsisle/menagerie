bbf7c8f82c5bb8107082e9cb83013e9ca8aef8e2
samsisle <samko9522@gmail.com>
2019-10-31 01:15:36 -0700

    ✨ Update solution.js

--- leet_code/0041 — First Missing Positive/solution.js
+++ leet_code/0041 — First Missing Positive/solution.js
@@ -4,17 +4,36 @@
  * Date   : 2019-07-29
  *
  * Time Complexity  : O(n)
- * Space Complexity : O(n)... this needs to be O(1)
+ * Space Complexity : O(1)
  */
 
 const performance = require('../../performance');
 
-function firstMissingPositive(nums) {
-  let map = new Map(); // using ES6 new Map
-  for (let num of nums) map.set(num, true); // stores the value by key ... map.set(key, value)
-  for (let i = 1; i <= Number.MAX_SAFE_INTEGER; i++) {
-    if (!map.has(i)) return i; // if the map doesn't have i, return i
+function firstMissingPositive(A) {
+  let i = 0;
+  let p = A.length;
+
+  while (i < p) {
+    // if correct number at index
+    if (A[i] === i + 1) {
+      i++;
+      // if number negative or zero, greater than p, or duplicate
+    } else if (A[i] <= 0 || A[i] > p || A[i] === A[A[i] - 1]) {
+      // swap current number with last number
+      swap(A, i, p - 1);
+      // we don't care about numbers at the end, so decrement p
+      p--;
+    } else {
+      // swap number to correct index
+      swap(A, i, A[i] - 1);
+    }
   }
+
+  return p + 1;
 }
 
-performance.test(() => firstMissingPositive([1, 2, 0]));
+function swap(arr, a, b) {
+  [arr[a], arr[b]] = [arr[b], arr[a]];
+}
+
+performance.test(() => firstMissingPositive([3, 4, -1, 1]));
