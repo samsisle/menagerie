f87eb0159c2bb15269bbbd941139618b379d7600
samsisle <samko9522@gmail.com>
2019-11-14 14:32:31 -0800

    ✨ Update solution.js

--- leet_code/0053 — Maximum Subarray/solution.js
+++ leet_code/0053 — Maximum Subarray/solution.js
@@ -1,20 +1,25 @@
 /**
  * Source : https://leetcode.com/problems/maximum-subarray/
  * Author : samsisle
- * Date   : 2019-08-17
+ * Date   : 2019-11-14
  *
  * Time Complexity  : O(n)
  * Space Complexity : O(1)
+ * 
+ * This is using Kadane's Algorithm (Dynamic Programming). 
  */
 
 function maxSubArray(A) {
-  if (A.length === 0) return 0;
-  let sum = 0;
+  if (!A.length) return 0;
 
-  return A.reduce((max, curr) => {
-    sum = curr + (sum > 0 ? sum : 0);
-    return Math.max(max, sum);
-  }, Number.NEGATIVE_INFINITY);
+  let maxSum = nums[0];
+
+  for (let i = 1; i < A.length; i++) {
+    if (A[i - 1] > 0) A[i] += A[i - 1];
+    maxSum = Math.max(A[i], maxSum);
+  }
+
+  return maxSum;
 }
 
 console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
