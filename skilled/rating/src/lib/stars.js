e206286b1ea2401409261400916cc703ad09f1ab
samsisle <samko9522@gmail.com>
2019-11-28 16:44:18 -0800

    Remove stars array

--- skilled/rating/src/lib/stars.js
+++
@@ -1,7 +0,0 @@
-export const stars = [
-  { id: 1, left: 0.5, right: 1 },
-  { id: 2, left: 1.5, right: 2 },
-  { id: 3, left: 2.5, right: 3 },
-  { id: 4, left: 3.5, right: 4 },
-  { id: 5, left: 4.5, right: 5 }
-];
