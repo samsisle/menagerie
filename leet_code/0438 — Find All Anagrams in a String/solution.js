6411b72e0680f55e0297c61da698d26ed601f67e
samsisle <samko9522@gmail.com>
2019-12-05 18:27:01 -0800

    ✨ Add solution.js

---
+++ leet_code/0438 — Find All Anagrams in a String/solution.js
@@ -0,0 +1,47 @@
+/**
+ * Source : https://leetcode.com/problems/valid-anagram/
+ * Author : samsisle
+ * Date   : 2019-12-5
+ *
+ * Time Complexity  : O(s + p)
+ * Space Complexity : O(1)
+ */
+
+function findAnagrams(S, P) {
+  let output = [];
+  let alphabet = new alphabet();
+
+  // map P's letter in alphabet first
+  for (let i = 0; i < P.length; i++) {
+    alphabet.add(P.charCodeAt(i) - 97);
+  }
+
+  for (let i = 0; i < S.length; i++) {
+    alphabet.remove(S.charCodeAt(i) - 97);
+
+    if (i >= P.length) {
+      alphabet.add(S.charCodeAt(i - P.length) - 97);
+    }
+    
+    if (alphabet.count === 0) {
+      output.push(i - P.length + 1);
+    }
+  }
+
+  return output;
+}
+
+function Alphabet() {
+  this.store = Array(26).fill(0);
+  this.count = 0;
+}
+
+Alphabet.prototype.add = function(pos) {
+  if (this.store[pos] === 0) this.count++;
+  this.store[pos]++;
+};
+
+Alphabet.prototype.remove = function(pos) {
+  this.store[pos]--;
+  if (this.store[pos] === 0) this.count--;
+};
