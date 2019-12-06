d8aef2533fe96d462834dc6db4d8b76c0661f945
samsisle <samko9522@gmail.com>
2019-12-05 17:58:23 -0800

    ✨ Update solution.js

--- leet_code/0242 — Valid Anagram/solution.js
+++ leet_code/0242 — Valid Anagram/solution.js
@@ -11,11 +11,10 @@ function isAnagram(s, t) {
   if (s.length !== t.length) return false;
 
   let alphabet = Array(26).fill(0);
-  const codeA = 'a'.charCodeAt(0);
 
   for (let i = 0; i < s.length; i++) {
-    alphabet[s.charCodeAt(i) - codeA]++;
-    alphabet[t.charCodeAt(i) - codeA]--;
+    alphabet[s.charCodeAt(i) - 97]++;
+    alphabet[t.charCodeAt(i) - 97]--;
   }
 
   return alphabet.every(letter => letter === 0);
