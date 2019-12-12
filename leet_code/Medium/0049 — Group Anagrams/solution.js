2bedabb1acfd4d707a106abcfd899d4f449dc7d8
samsisle <samko9522@gmail.com>
2019-12-11 16:43:21 -0800

    ✨ Update solution.js

--- leet_code/Medium/0049 — Group Anagrams/solution.js
+++ leet_code/Medium/0049 — Group Anagrams/solution.js
@@ -8,35 +8,8 @@
  */
 
 // 26 primes to go with 26 letters in the alphabet
-const primes = [
-  2,
-  3,
-  5,
-  7,
-  11,
-  13,
-  17,
-  19,
-  23,
-  29,
-  31,
-  37,
-  41,
-  43,
-  47,
-  53,
-  59,
-  61,
-  67,
-  71,
-  73,
-  79,
-  83,
-  89,
-  97,
-  101,
-  107
-];
+// prettier-ignore
+const primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,107];
 
 // this function will generate a unique key for each anagram
 function generateKey(S) {
