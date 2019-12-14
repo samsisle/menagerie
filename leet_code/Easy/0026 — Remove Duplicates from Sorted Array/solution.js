404f7eef392252f4fb5a4302a0bc76c29ad25070
samsisle <samko9522@gmail.com>
2019-12-13 16:37:30 -0800

    ✨ Update solution.js

--- leet_code/Easy/0026 — Remove Duplicates from Sorted Array/solution.js
+++ leet_code/Easy/0026 — Remove Duplicates from Sorted Array/solution.js
@@ -28,11 +28,11 @@ const performance = require('../../../performance');
  */
 
 function removeDuplicates(nums) {
-  let i = 0;
-  for (let j = 1; j < nums.length; j++) {
+  for (let i = 0, j = 1; j < nums.length; j++) {
     if (nums[j] === nums[j - 1]) i++;
     else nums[j - i] = nums[j];
   }
+  
   return nums.length - i;
 }
 
