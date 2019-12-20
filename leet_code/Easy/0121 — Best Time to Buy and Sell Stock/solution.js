1f120df71744e22ff203a77a8ee263c40775e010
samsisle <samko9522@gmail.com>
2019-12-20 00:26:11 -0800

    📝 Add README.md

---
+++ leet_code/Easy/0121 — Best Time to Buy and Sell Stock/solution.js
@@ -0,0 +1,20 @@
+/**
+ * Source : https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+ * Author : samsisle
+ * Date   : 2019-12-22
+ *
+ * Time Complexity  : O(n)
+ * Space Complexity : O(1)
+ */
+
+function maxProfit(prices) {
+  let minPrice = Infinity;
+  let max = 0;
+
+  for (let price of prices) {
+    minPrice = Math.min(minPrice, price);
+    max = Math.max(max, price - minPrice);
+  }
+
+  return maxProfit;
+}
