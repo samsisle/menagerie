741f73098eea8820525976fc0f9eb21482034d44
samsisle <samko9522@gmail.com>
2020-01-07 00:27:22 -0800

    Update solution.js

--- leet_code/Easy/0283 — Move Zeros/solution.js
+++ leet_code/Easy/0283 — Move Zeros/solution.js
@@ -1,22 +1,22 @@
 /**
  * Source : https://leetcode.com/problems/move-zeroes/
  * Author : samsisle
- * Date   : 2019-08-14
+ * Date   : 2020-01-07
  *
  * Time Complexity  : O(n)
  * Space Complexity : O(1)
  */
 
 function moveZeros(nums) {
-  // p equals pointer
   let p = 0;
+
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] !== 0) {
-      nums[p] = nums[i];
-      nums[i] = p === i ? nums[i] : 0;
+      [nums[p], nums[i]] = [nums[i], nums[p]];
       p++;
     }
   }
+
   return nums;
 }
 
