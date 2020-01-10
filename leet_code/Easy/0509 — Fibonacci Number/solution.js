cf034faaa2e030ffb73353d8ed5c1f539935e59b
samsisle <samko9522@gmail.com>
2020-01-09 23:37:04 -0800

    Update solution.js

--- leet_code/Easy/0509 — Fibonacci Number/solution.js
+++ leet_code/Easy/0509 — Fibonacci Number/solution.js
@@ -1,27 +1,23 @@
 /**
  * Source : https://leetcode.com/problems/fibonacci-number/
  * Author : samsisle
- * Date   : 2019-07-28
+ * Date   : 2020-01-11
  *
  * Time Complexity  : O(n)
  * Space Complexity : O(n)
  */
 
-const performance = require('../../../performance');
+function fibbo(n) {
+  const memo = {};
 
-function fib(n) {
-  let memo = {};
   for (let i = 0; i <= n; i++) {
-    if (i <= 1) memo[i] = i;
-    else memo[i] = memo[i - 1] + memo[i - 2];
+    if (i <= 1) {
+      memo[i] = i;
+    } else {
+      memo[i] = memo[i - 1] + memo[i - 2];
+    }
   }
   return memo[n];
 }
 
-// Slow as hell recursive method
-// function fib(num) {
-//   if (num <= 1) return 1;
-//   return fib(num - 1) + fib(num - 2);
-// }
-
-performance.test(() => fib(42));
+console.log(fibbo(42));
