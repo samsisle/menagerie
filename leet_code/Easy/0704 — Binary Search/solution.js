7cf13278b864cba0d18568632e2f523759a9cdf3
samsisle <samko9522@gmail.com>
2020-01-20 13:23:06 -0800

    Update solution.js

--- leet_code/Easy/0704 — Binary Search/solution.js
+++ leet_code/Easy/0704 — Binary Search/solution.js
@@ -7,22 +7,23 @@
  * Space Complexity :  O(1)
  */
 
-const performance = require('../../../performance');
-
 function binarySearch(nums, target) {
-  // it's a great strategy to visualize things with 'pointers'
-  // start pointer and end pointer to log indexes
   let start = 0;
   let end = nums.length - 1;
 
   while (start <= end) {
-    // I'm actually not sure why both ceiling and floor works...
     const mid = Math.floor((start + end) / 2);
-    if (target < nums[mid]) end = mid - 1;
-    else if (target > nums[mid]) start = mid + 1;
-    else return mid;
+
+    if (target < nums[mid]) {
+      end = mid - 1;
+    } else if (target > nums[mid]) {
+      start = mid + 1;
+    } else {
+      return mid;
+    }
   }
+
   return -1;
 }
 
-performance.test(() => binarySearch([-1, 0, 3, 5, 9, 12], 9));
+binarySearch([-1, 0, 3, 5, 9, 12], 9);
